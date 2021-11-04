b9fe14e4c23fc3481cec60154c8175fe512ba6fd
GarifullinII <ildaridrisovich@gmail.com>
2021-11-04 16:56:54 +0400

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import TestClass from './TestClass';
+// import TestClass from './TestClass';
 import TestFunction from './TestFunction';
 
 function App() {
   return (
    <>
-    <TestClass argument={1000}/>
-    <TestFunction />
+    {/* <TestClass argument={1000}/> */}
+    <TestFunction argument={1000000}/>
    </>
   );
 }
--- src/TestFunction.js
+++ src/TestFunction.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function TestFunction() {
+function TestFunction(props) {
+	
+	console.log('set state');
+	const [s1, setS1] = React.useState(props.argument);
+
+	const buttonHandler = () => {
+		console.log('work buttonHadler');
+
+		let val = s1;
+		val ++;
+		setS1(val);
+	}
+
+	// принимает функцию, которая срабатывает при эффектах внутри компонента, 
+	useEffect(() => {
+		console.log('effect');
+	});
+	
+	console.log('render 1-1')
 	return (
 	<>
-	</>)
+		{console.log('render 2-2')}
+		<div>
+			{s1}
+		</div>
+		<button onClick={buttonHandler}>
+			Click
+		</button>
+	</>
+	)
 }
 
 export default TestFunction;
