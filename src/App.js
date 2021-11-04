79f720f94537e2eaef51daacff0a96c05d987084
GarifullinII <ildaridrisovich@gmail.com>
2021-11-04 16:23:36 +0400

    add functional component TestFunction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import TestClass from './TestClass';
+import TestFunction from './TestFunction';
 
 function App() {
   return (
    <>
     <TestClass argument={1000}/>
+    <TestFunction />
    </>
   );
 }
---
+++ src/TestFunction.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TestFunction() {
+	return (
+	<>
+	</>)
+}
+
+export default TestFunction;
